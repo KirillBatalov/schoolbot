e7940b1b37e990ac3ff730e339cf6dd37b23209c
Андрей Боев <a.boev.it@gmail.com>
2023-10-22 10:39:46 +0300

    Мелкие правки

--- db_controller.py
+++ db_controller.py
@@ -2,20 +2,24 @@ import pymysql
 
 
 class DatabaseController:
+    connection = pymysql.connect(host='95.163.233.154',
+                                 port=3310,
+                                 user='kirill.b',
+                                 passwd='userkirill2023',
+                                 db='kirill_batalov')
     @staticmethod
     def get_grades():
-        connection = pymysql.connect(host='95.163.233.154',
-                                     port=3310,
-                                     user='kirill.b',
-                                     passwd='userkirill2023',
-                                     db='kirill_batalov')
-        with connection:
+        with DatabaseController.connection as connection:
             cursor = connection.cursor()
-            query = 'select * from grade ' +\
-                    'order by grade_value, grade_name'
-            cursor.execute(query)
+            cursor.execute('select * from grade '
+                           'order by grade_value, grade_name')
             rows = cursor.fetchall()
         return dict((f'{row[1]}-{row[2]}', row[0]) for row in rows)
+
+    # TODO: Метод, определяющий, есть ли пользователь с данным id в БД, если есть, то возвращающий класс User,
+    #       в противном случае None
+
+    # TODO: Метод, добавляющий пользователя в БД (id, grade)
 
 
 print(DatabaseController.get_grades())
